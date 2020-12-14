1e8cae03321697b8f6cb66d401d7465b4f271d8a
Andrew <andrewurom@gmail.com>
2020-12-14 22:25:56 +0100

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/redux/cart/cart.action.js
+++ src/redux/cart/cart.action.js
@@ -4,4 +4,9 @@ import CartActionTypes  from "./cart.types";
 export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN,
 });
+
+export const addItem = items => ({
+  type: CartActionTypes.ADD_ITEM,
+  payload: items,
+})
  
